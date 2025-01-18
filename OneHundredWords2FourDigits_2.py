61635d8d610bec778ab4e782e988ec8e7971f9f4
one61803 <92607101+one61803@users.noreply.github.com>
2025-01-18 10:25:02 -0600

    L259, L263, L271, L275:
    Add safety condition and error return value to if_page_then_swallow and if_paragraph_then_swallow functions.

--- OneHundredWords2FourDigits_2.py
+++ OneHundredWords2FourDigits_2.py
@@ -256,10 +256,11 @@ def if_page_then_swallow(a_string):
     if (a_string[0:3] == "P. ") and (a_string[3] in digits_LS):
         a_string = a_string[3:]
         a_string = swallow_number(a_string)
-        if (a_string[0] == " "):
+        if (len(a_string) > 0) and (a_string[0] == " "):
             a_string = a_string[1:]
         else:
             print("L262. ERROR in if_page_then_swallow: No space after the page number.")
+            a_string = ""
     return a_string
 
 def if_paragraph_then_swallow(a_string):
@@ -267,10 +268,11 @@ def if_paragraph_then_swallow(a_string):
     if (a_string[0:5] == "PAR. ") and (a_string[5] in digits_LS):
         a_string = a_string[5:]
         a_string = swallow_number(a_string)
-        if (a_string[0] == " "):
+        if (len(a_string) > 0) and (a_string[0] == " "):
             a_string = a_string[1:]
         else:
-            print("L273. ERROR in if_paragraph_then_swallow: No space after the paragraph number.")
+            print("L274. ERROR in if_paragraph_then_swallow: No space after the paragraph number.")
+            a_string = ""
     return a_string
 
 def letter_count(a_string):
