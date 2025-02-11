7eac23e3ef527cae5804a814a0084ce8c648e820
one61803 <92607101+one61803@users.noreply.github.com>
2025-02-10 18:51:21 -0600

    L407 in "mode: alter":
    change disjunction to conjunction

--- FourHundredWords2SixteenUALPHAnumbers_2.py
+++ FourHundredWords2SixteenUALPHAnumbers_2.py
@@ -404,7 +404,7 @@ for i in range(1, upper_bound + 1):
                 print("Fingerprint not ok; try again.")
     elif (mode == "a"):
         "mode: alter"
-        if not (i in [100, 200, 300]) or not (is_toy_version and (i in [10, 20, 30])):
+        if not (i in [100, 200, 300]) and not (is_toy_version and (i in [10, 20, 30])):
             print("\nLocation #" + str(i) + ": " + data[i][0])
             OK = False
             while not OK:
